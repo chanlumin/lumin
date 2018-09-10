f331d7e0640cc2859015d6b898641c6930d4b831
chenlumin <chenlumin@360buyad.local>
2018-09-10 21:16:00 +0800

    添加routerh和stylus loader

---
+++ src/container/Home/index.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+import '../../assets/stylus/reset.styl'
+
+class Home extends Component {
+  render() {
+    return <div>Home</div>
+  }
+}
+
+export default Home 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-import React from 'react';
+ import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './assets/stylus/reset.styl'
+import {BrowserRouter, Route} from 'react-router-dom'
+import Home from './container/Home/index'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+ReactDOM.render(
+<BrowserRouter>
+  <div>
+    <Route path='/' component={Home}></Route>
+  </div>
+</BrowserRouter>, document.getElementById('root'));
